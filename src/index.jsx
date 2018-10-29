b08c086bb9dbfe3a0ea0d12d91a3898b722e5916
Mohammed Faizuddin <faiz@avantari.org>
2018-10-29 20:12:55 +0530

    initial commit flow-app-component-directory

---
+++ src/index.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+
+import AppComponent from 'flow-app-component';
+
+class DirectoryComponent extends AppComponent {
+  constructor() {
+    super();
+    const newState = {
+      properties: [
+        {
+          categoryName: 'General',
+          categoryDescription: 'App Name',
+          properties: [],
+        },
+        {
+          categoryName: 'Access Control',
+          categoryDescription: 'Control access to the directory',
+          properties: [
+            {
+              id: 'allowed-roles',
+              name: 'Allowed Roles',
+              type: 'roles',
+              description: 'Select roles that have access to this directory',
+              value: 'all',
+              options: {
+                multi: true,
+              },
+            },
+          ],
+        },
+      ],
+      iconUrl: '/assets/images/text-component.svg',
+      name: 'Directory',
+      type: 'ui-component',
+      category: 'Wrappers',
+      parent: null,
+      showOnComponentsPanel: false,
+      isValuable: true,
+      allowsChildren: true,
+    };
+
+    this.state = Object.assign(this.state, newState); // merge two states together, and dont lose any parent state properties.
+  }
+
+  renderContent() {
+    return null;
+  }
+}
+
+export default DirectoryComponent;
